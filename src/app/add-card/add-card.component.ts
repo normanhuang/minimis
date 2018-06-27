80c7d8e33a0fae4af4ff6fd5d502c47311f9f223
Huang <normanhuang@quickenloans.com>
2018-06-27 15:14:40 -0400

    changes

--- src/app/add-card/add-card.component.ts
+++ src/app/add-card/add-card.component.ts
@@ -5,7 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add-card.component.html',
   styleUrls: ['./add-card.component.css']
 })
+
 export class AddCardComponent implements OnInit {
+
+  AddPlaces(place: string, places: string[]) {
+    places.push(place);
+    return places;
+  }
 
   constructor() { }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   places$: string[];
-  constructor() {
+  constructor(private weatherService: WeatherService) {
+    this.weatherService.getCurrentTemp('Paris');
    }
 
   ngOnInit() {
-    this.places$ = ['paris', 'finland'];
   }
 
 }
